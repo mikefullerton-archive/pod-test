87a3b49e74dff1cc617edc00862792cca0e61b89
Mike Fullerton <mike@Mikes-MacPro.local>
2013-11-25 08:12:04 -0800

    Initial Commit

---
+++ Test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Mike Fullerton on 11/25/13.
+//  Copyright (c) 2013 GreenTongue Software, LLC, Mike Fullerton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
